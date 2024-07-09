9c891a5ecc52117f74fdcc75193ed1dc8fde971b
yosep98 <125348862+yosep98@users.noreply.github.com>
2024-07-09 11:35:25 +0900

    [Bronze IV] Title: R2, Time: 172 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int R2= 0;
+        Scanner sc = new Scanner(System.in);
+        int R1 =sc.nextInt();
+        int S= sc.nextInt();
+        R2=(S*2)-R1;
+
+        System.out.println(R2);
+
+    }
+}
