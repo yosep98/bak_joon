96536d903eabe5924b49591fb96b23983217065a
yosep98 <125348862+yosep98@users.noreply.github.com>
2024-09-12 23:08:36 +0900

    [level 1] Title: 폰켓몬, Time: 6.95 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        Set set=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+        set.add(nums[i]);    
+        }
+        int answer = set.size();
+        if(answer>nums.length/2)
+        answer=nums.length/2;
+        return answer;
+    }
+}
