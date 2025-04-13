1b2c399b5368b074cc281b7c37f51d92988b4586
yosep98 <125348862+yosep98@users.noreply.github.com>
2025-04-13 18:39:39 +0900

    [Silver V] Title: 귀찮음, Time: 1440 ms, Memory: 311320 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.java
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -5,24 +5,24 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		long sum=0;
+		long corret = 0;
 		ArrayList<Integer> list = new ArrayList<>();
 		// 배열 순서대로 저장
 		for (int i = 0; i < n; i++) {
-			list.add(sc.nextInt());
+			int num = sc.nextInt();
+			list.add(num);
+			sum+=num;
 		}
 		// 정렬
-		int corret = 0;
 		list.sort(Integer::compareTo);
 		for (int i = 0; i < n; i++) {
 			int num1 = list.get(i);
-			int sum = 0;
-			for (int j = i + 1; j < n; j++) {
-				sum += list.get(j);
+			sum-=num1;
+			corret+=num1*(sum);
 			}
-			corret += num1 * sum;
-
-		}
 		System.out.println(corret);
+		}
 	}
 
-}
+
